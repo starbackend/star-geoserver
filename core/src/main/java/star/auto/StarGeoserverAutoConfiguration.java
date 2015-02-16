f15192365995d72eca218d6ca13c7cf048e7960e
maprohu <maprohu@gmail.com>
2015-02-16 17:56:22 +0000

    added config annotation

--- core/src/main/java/star/auto/StarGeoserverAutoConfiguration.java
+++ core/src/main/java/star/auto/StarGeoserverAutoConfiguration.java
@@ -1,9 +1,11 @@
 package star.auto;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import star.geoserver.StarGeoserverConfiguration;
 
+@Configuration
 @Import(StarGeoserverConfiguration.class)
 public class StarGeoserverAutoConfiguration {
 
