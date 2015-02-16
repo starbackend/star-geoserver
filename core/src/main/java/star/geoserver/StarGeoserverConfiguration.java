e014f490f690e5d15cc42a987d4395eb20ec8440
maprohu <maprohu@gmail.com>
2015-02-16 17:47:18 +0000

    refer to cwatch-geoserver

--- core/src/main/java/star/geoserver/StarGeoserverConfiguration.java
+++ core/src/main/java/star/geoserver/StarGeoserverConfiguration.java
@@ -1,10 +1,13 @@
 package star.geoserver;
 
+import org.cwatch.geoserver.CwatchGeoserverConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource({"classpath:/star-geoserver.properties"})
+@Import(CwatchGeoserverConfiguration.class)
 public class StarGeoserverConfiguration {
 
 }
